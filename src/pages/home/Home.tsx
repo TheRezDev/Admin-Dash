7f3dd71d32c36767ac2493f495c287486ffefda4
Reza.dev <the.rez.dev@gmail.com>
2023-08-05 08:59:54 +0330

    feature/pie&bar charts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,8 @@
+import BarChartBox from '../../components/barChartBox/barCahrtBox';
 import ChartBox from '../../components/chartBox/ChartBox';
+import PieChartBox from '../../components/pieChartBox/PieChartBox';
 import TopBox from '../../components/topBox/TopBox';
+import { barChartBoxRevenue, barChartBoxVisit } from '../../data';
 import './home.scss';
 const Home = () => {
     return ( 
@@ -9,12 +12,12 @@ const Home = () => {
         </div>
         <div className="box box2"><ChartBox/></div>
         <div className="box box3"><ChartBox/></div>
-        <div className="box box4"><ChartBox/></div>
+        <div className="box box4"><PieChartBox/></div>
         <div className="box box5"><ChartBox/></div>
         <div className="box box6">Box6</div>
         <div className="box box7">Box7</div>
-        <div className="box box8">Box8</div>
-        <div className="box box9">Box9</div>
+        <div className="box box8"><BarChartBox {...barChartBoxVisit}/></div>
+        <div className="box box9"><BarChartBox {...barChartBoxRevenue}/></div>
     </div> 
     );
 };
